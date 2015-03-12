2cf0124c3af95c8508ddd750b66543d729ac7114
Victor Soto Barba <vsbarba@gmail.com>
2015-03-12 09:38:17 +0800

    rubaxa JS updated

--- application/js/rubaxa.js
+++ application/js/rubaxa.js
@@ -28,7 +28,10 @@
         sort: true,
         group: {
           name: 'gridlist'
-        }
+        },
+        scroll: true, // or HTMLElement
+        scrollSensitivity: 80, // px, how near the mouse must be to an edge to start scrolling.
+        scrollSpeed: 30, // px
       }
 
 
@@ -53,7 +56,7 @@
       $scope.grid = {
         id: null,
         elementId: 'element_id',
-        class: null,
+        class: 'root',
         children: [],
         attributes: {},
         widgetSections: [],
@@ -91,6 +94,59 @@
             $compile(element.contents())(scope);
             scope.$broadcast('iframe::doneCompiling');
           }, 1000);
+
+        }
+      };
+    }
+  ])
+
+  .directive('pageBuilder', [
+    'grid.page',
+    'grid.data',
+    'grid.add',
+    '$compile',
+    '$timeout',
+    function(Page, Grid, GridAdd, $compile, $timeout) {
+      'use strict';
+
+      return {
+        restrict: 'E',
+        require: '^ngModel',
+        scope: true,
+        template: '<div ng-include="templateUrl"></div>',
+        link: function(scope, element, attrs, ngModel) {
+
+          var ngModelItem = scope.$eval(attrs.ngModel);
+          var gridParent = scope.$eval(attrs.gridParent);
+          var gridOptions = scope.$eval(attrs.gridOptions);
+          var pageBuilderTpl = attrs.pageBuilderTpl;
+
+          scope.ngModelItem = ngModelItem;
+          scope.gridParent = gridParent;
+          scope.gridOptions = gridOptions;
+          scope.templateUrl = pageBuilderTpl;
+
+          var off = 0;
+
+          scope.removeCurrentGrid = function(gridParent, index) {
+            gridParent.splice(index, 1);
+          }
+
+          scope.addParentChild = function(parent, currentItem) {
+            console.log('parent', parent);
+            var gridTpl = angular.copy(GridAdd);
+            gridTpl.id = off++;
+            parent.children.push(gridTpl);
+            gridTpl = {};
+          };
+
+          scope.removeParentChild = function(parent, data) {
+            if (parent.length > 0) {
+              parent.splice(data, 1);
+            } else {
+              console.info('Element has no child');
+            }
+          };
 
         }
       };
@@ -221,5 +277,66 @@
       'io-disable-grid-drop': true
     }
   }])
+
+  .constant('grid.data', {
+    id: null,
+    elementId: 'container',
+    class: 'container',
+    children: [],
+    widgets: [],
+    attributes: {},
+    widgetSections: [],
+  })
+
+  .constant('grid.add', {
+    id: null,
+    elementId: 'elementId',
+    class: null,
+    children: [],
+    widgets: [],
+    attributes: {},
+    widgetSections: [],
+  })
+
+  .constant('grid.page', {
+    name: null,
+    description: null,
+    iconUrl: null,
+    customHeadTags: [],
+    category: null,
+    tags: [],
+    grid: {
+      id: null,
+      elementId: 'element_id',
+      class: null,
+      children: [],
+      widgets: [{
+        id: 1,
+        name: 'Widget Name'
+      }, {
+        id: 2,
+        name: 'Widget Name 2'
+      }, {
+        id: 3,
+        name: 'Widget Name 3'
+      }],
+      attributes: {},
+      widgetSections: [],
+    },
+    assets: {
+      _stylesCompiled: null,
+      _scriptsCompiled: null,
+      _scriptsLibrary: [],
+      _stylesLibrary: []
+    },
+    headSection: {
+      docType: null,
+      meta: {
+        author: null,
+        description: null,
+        keywords: []
+      }
+    },
+  })
 
 }());
